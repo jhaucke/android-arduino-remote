34594e4c06652b4b24f43e3ee6c5a09943c02a54
jere0710 <Jere@jeres-macbook.fritz.box>
2014-01-20 20:25:04 +0100

    add script for udpserver

---
+++ UdpServer/bridgeUdpServer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys
+import socket
+
+sys.path.insert(0, '/usr/lib/python2.7/bridge/')
+ 
+from bridgeclient import BridgeClient as bridgeclient
+
+value = bridgeclient()
+
+print 'server is running...'
+
+UDP_IP = "192.168.178.32"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+                     
+while True:
+  data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+  value.put('D13',data)
